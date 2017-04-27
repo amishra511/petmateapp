54483a72d409e7656eda55f9cb8c4bb462ca14cb
Komal Sudan <Komal Sudan>
2017-04-26 22:05:58 -0400

    Deleted and added project files again

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+import { enableProdMode } from '@angular/core';
 
 // Enable production mode unless running locally
 if (!/localhost/.test(document.location.host)) {
   enableProdMode();
 }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
